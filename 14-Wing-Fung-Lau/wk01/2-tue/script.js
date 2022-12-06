78b590e1ed8470ea1edb7c0455a7f09037013e23
Wing Fung Lau <wingfunglau@Wings-MacBook-Pro.local>
2022-12-06 16:32:58 +1100

    Section 2

--- 14-Wing-Fung-Lau/wk01/2-tue/script.js
+++ 14-Wing-Fung-Lau/wk01/2-tue/script.js
@@ -3,3 +3,11 @@ let age = 19
 if (age <= 19) {
     console.log("You're still a teenager!")
 }
+
+//Section 2
+let a = 51
+if (a%2 === 0) {
+    console.log("After running our calculations, the result is even")
+} else {
+    console.log("After running our calculations, the result is odd")
+}
