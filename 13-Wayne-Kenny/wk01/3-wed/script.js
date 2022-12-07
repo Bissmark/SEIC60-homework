2a35c9e1901a6103d822a10cedd80bcc022820ca
Wayne Kenny <waynekenny@Waynes-MacBook-Air.local>
2022-12-08 09:33:14 +1100

    updated homework

--- 13-Wayne-Kenny/wk01/3-wed/script.js
+++ 13-Wayne-Kenny/wk01/3-wed/script.js
@@ -45,7 +45,28 @@
 // }
 // console.log(helloIn('Malay'))
 
-//Leap Year
-// for (let i = 1924; i < 2024; i += 4) {
-//     console.log(i)
+// //Leap Year
+// for (let i = 1924; i < 2024; i += 4)
+// //     console.log(i)
+// // }
+
+//// Leap Year Version 2 - doesn't work
+// const range = function (st_year, end_year) {   
+//     for (let i = st_year; i <= end_year; i++) {
+//     }
+//     }
+//  const leapYear = function (year) {
+//     if ((year % 4 === 0 && year % 100 !== 0) || (year % 100 === 0 && year % 400 === 0)) {
+//         return year;
+//     }
 // }
+// console.log(range(1924,2024));
+
+//// Leap Year version 3 (works)
+// (function(startY, endY){
+//     for(let i = startY; i <= endY; i++){
+//     if((i % 4 === 0 && i % 100 !== 0) || (i % 100 === 0 && i % 400 === 0)){
+//         console.log(i)
+//     }
+//     }
+//     })(1924, 2020)
