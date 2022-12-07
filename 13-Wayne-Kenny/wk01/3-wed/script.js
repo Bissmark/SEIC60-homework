a0229a4dee5b16f9f066a5a5b2872d4633c82f19
Wayne Kenny <waynekenny@Waynes-MacBook-Air.local>
2022-12-07 18:05:50 +1100

    Homework for Wayne

---
+++ 13-Wayne-Kenny/wk01/3-wed/script.js
@@ -0,0 +1,51 @@
+// Calculate
+// const calculate = function(num1, num2, operation) {
+//     if (operation === 'add') {
+//         return num1 + num2
+//     }
+//     else if (operation === 'sub') {
+//         return num1 - num2
+//     }
+//     else if (operation === 'mult') {
+//         return num1 * num2
+//     }
+//     else if (operation === 'div') {
+//         return num1/num2
+//     }
+//     else if (operation === 'exp') {
+//         return  (num1 ** num2)
+//     }
+// }
+// console.log(calculate(4, 2, 'add'))
+// console.log(calculate(4, 2, 'sub'))
+// console.log(calculate(4, 2, 'mult'))
+// console.log(calculate(4, 2, 'div'))
+// console.log(calculate(4, 2, 'exp'))
+
+// // helloIn
+// const helloIn = function(languageName) {
+//     if (languageName === 'English') {
+//         return 'Hello'
+//     }
+//     else if (languageName === 'German') {
+//         return 'Hallo'
+//     }
+//     else if (languageName === 'Portugese') {
+//         return 'Ola'
+//     }
+//     else if (languageName === 'French') {
+//         return 'Bonjour'
+//     }
+//     else if (languageName === 'Dog') {
+//         return  'Woof'
+//     }
+//     else {
+//         return `Sorry I don't speak ${languageName}` 
+//     }
+// }
+// console.log(helloIn('Malay'))
+
+//Leap Year
+// for (let i = 1924; i < 2024; i += 4) {
+//     console.log(i)
+// }
