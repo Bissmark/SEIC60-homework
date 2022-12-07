ff8a9e17cb64fd6a3affa85e66acdd7c9104109f
Wing Fung Lau <wingfunglau@Wings-MacBook-Pro.local>
2022-12-07 16:41:06 +1100

    helloIn

--- 14-Wing-Fung-Lau/wk01/3-wed/functions.js
+++ 14-Wing-Fung-Lau/wk01/3-wed/functions.js
@@ -1,25 +1,39 @@
-//calculate
+// //calculate
 
-const calculate = function(num1, num2, operation){
-    if (operation === "add"){
-        return num1 + num2
-    }
-    if (operation === "sub"){
-        return num1 - num2
-    }
-    if (operation === "mult"){
-        return num1 * num2
-    }
-    if (operation === "div"){
-        return num1 / num2
-    }
-    if (operation === "exp"){
-        return num1 ** num2
+// const calculate = function(num1, num2, operation){
+//     if (operation === "add"){
+//         return num1 + num2
+//     }
+//     if (operation === "sub"){
+//         return num1 - num2
+//     }
+//     if (operation === "mult"){
+//         return num1 * num2
+//     }
+//     if (operation === "div"){
+//         return num1 / num2
+//     }
+//     if (operation === "exp"){
+//         return num1 ** num2
+//     }
+// }
+
+// console.log(calculate(4, 2, 'add')) // -> 6
+// console.log(calculate(4, 2, 'sub')) // -> 2
+// console.log(calculate(4, 2, 'mult')) // -> 8
+// console.log(calculate(4, 2, 'div')) // -> 2
+// console.log(calculate(4, 2, 'exp')) // -> 16
+
+//helloIn
+
+const helloIn = function(lang) {
+    const recognisedLang = {"english":"Hello","german":"Hallo","portuguese":"Olá",
+"french":"Bonjour"}
+    if (recognisedLang[lang] == undefined) {
+        return "Sorry, I don't speak."
+    } else {
+        return recognisedLang[lang]
     }
 }
 
-console.log(calculate(4, 2, 'add')) // -> 6
-console.log(calculate(4, 2, 'sub')) // -> 2
-console.log(calculate(4, 2, 'mult')) // -> 8
-console.log(calculate(4, 2, 'div')) // -> 2
-console.log(calculate(4, 2, 'exp')) // -> 16
+console.log(helloIn(prompt().toLowerCase()))
