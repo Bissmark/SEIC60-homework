156328f3d65b9f1051dbb9293ae23792bcb4977c
Wing Fung Lau <wingfunglau@Wings-MacBook-Pro.local>
2022-12-08 17:20:19 +1100

    The 3rd item is potato bake!

--- 14-Wing-Fung-Lau/wk01/4-thu/script.js
+++ 14-Wing-Fung-Lau/wk01/4-thu/script.js
@@ -1,9 +1,33 @@
-//Where's Waldo
-const whereIsWaldo = [["Timmy", "Frank"], "Eggbert",
-                      ["Lucinda", "Jacc", "Neff", "Snoop"],
-                      ["Petunia", ["Baked Goods", "Waldo"]]]
-whereIsWaldo.splice(1,1)
-console.log(whereIsWaldo)
-whereIsWaldo[1][2] = "No One"
-console.log(whereIsWaldo)
-console.log(whereIsWaldo[2][1][1])
+// //Where's Waldo
+// const whereIsWaldo = [["Timmy", "Frank"], "Eggbert",
+//                       ["Lucinda", "Jacc", "Neff", "Snoop"],
+//                       ["Petunia", ["Baked Goods", "Waldo"]]]
+// whereIsWaldo.splice(1,1)
+// console.log(whereIsWaldo)
+// whereIsWaldo[1][2] = "No One"
+// console.log(whereIsWaldo)
+// console.log(whereIsWaldo[2][1][1])
+
+//Array Restaurant
+const menu = ['chicken',
+    'apple pie',
+    'soup',
+    'steak',
+    'salad',
+    'turkey sandwich',
+    'potato bake']
+menu.sort()
+console.log(menu)
+menu.shift()
+menu.splice(1,1)
+console.log(menu)
+
+const choice = prompt("what would you like to eat?").toLowerCase()
+const checking = menu.filter(function(any){
+    return any === choice
+})
+if (choice === checking[0]){
+    console.log(`Sure, the ${choice} will be served in 15 minutes`)
+    } else {
+        console.log("Sorry, we don't have that")
+    }
