5a1d3487c3c0280a58e550b8dc4d11a145035026
Débora Jardim <debor@MacDebora.local>
2022-12-08 17:06:45 +1100

    adding wednesday homework

---
+++ 03-Debora-Jardim/wk01/3-wed/functions.js
@@ -0,0 +1,17 @@
+//HOMEWORK WED
+
+
+const helloIn = function (language){
+    let answer = `any`
+if (language === `portuguese`){
+    return (`ola`)
+} if (language === `spanish`){
+    return (`hola`)
+} if (language === `english`){
+    return (`hello`)
+}
+}
+console.log(helloIn(`portuguese`));
+console.log(helloIn(`spanish`));
+console.log(helloIn(`english`));
+
