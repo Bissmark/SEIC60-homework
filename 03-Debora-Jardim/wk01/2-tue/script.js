f8b10d8abefca3f971a9dff6f5cc970bbb6913ec
Débora Jardim <debor@MacDebora.local>
2022-12-08 17:14:51 +1100

    submitting conditional homework

---
+++ 03-Debora-Jardim/wk01/2-tue/script.js
@@ -0,0 +1,47 @@
+// //HOMEWORK DAY 02 - not sure if should keep writing in this folder
+
+// //Section 01
+const DebAge = 35 
+if(DebAge <= 40){
+    console.log(`u r still a teenager`)
+}
+
+// //Section 02
+let a = 10
+if(a % 2 === 0){
+    console.log(`After running our calculations, the result is even`)
+}else{
+    console.log(`After running our calculations, the result is odd`)
+}
+
+//Section 03
+let b = 20
+if(b <= 25){
+    console.log(`bottom quartile`)
+}if(b > 25 <= 50){
+    console.log(`lower quartile`)
+}if(b < 50 >= 75){
+   console.log(`upper quartile`)
+}if(b > 75 <= 100){
+    console.log(`top quartile`)
+}
+
+//Section 04 ----> prompt() not sure 
+
+
+//Section 05 
+const noMove = `no`
+const yesMove = `yes`
+let shouldIt = `yes`
+
+if(yesMove === `yes`){
+}if(shouldIt === `yes`){
+console.log(`no problem`)
+}else{
+    console.log(`seller tape!`)
+}if(noMove === `no`){
+}if(shouldIt === `yes`){
+console.log(`fix it`)
+    }else{
+        console.log(`no problem!`)   
+}
