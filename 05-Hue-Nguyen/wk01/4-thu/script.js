3e750863d888a604784b9bb0a6d001be1e5b7b17
HueNguyen <nguyenhue.se@gmail.com>
2022-12-09 00:14:45 +1100

    optimise solution for the solar system part 2 / print sum of the number of satelittes

--- 05-Hue-Nguyen/wk01/4-thu/script.js
+++ 05-Hue-Nguyen/wk01/4-thu/script.js
@@ -103,161 +103,161 @@
 // }
 
 // // // The solar system part 2
-// const system = {
-//     Mercury:
-//     {
-//         radiusp: 'same ',
-//         name: 'Mercury',
-//         density: 5.43,
-//         tilt: 2,
-//         image: 'textures/mercury.gif',
-//         rotationperiod: 1408,
-//         period: 0.24,
-//         radiuse: 2439,
-//         satellites: 0,
-//         au: 0.3871,
-//         eccentricity: 0.206,
-//         velocity: 47.89,
-//         mass: 0.06,
-//         inclination: 7
-//     },
-//     Venus:
-//     {
-//         radiusp: 'same ',
-//         name: 'Venus',
-//         density: 5.25,
-//         tilt: 177.3,
-//         image: 'textures/venus.gif',
-//         rotationperiod: 5832,
-//         period: 0.62,
-//         radiuse: 6052,
-//         satellites: 0,
-//         au: 0.7233,
-//         eccentricity: 0.007,
-//         velocity: 35.04,
-//         mass: 0.82,
-//         inclination: 3.4
-//     },
-//     Earth:
-//     {
-//         radiusp: 6357,
-//         name: 'Earth',
-//         density: 5.52,
-//         tilt: 23.45,
-//         image: 'textures/earth.gif',
-//         rotationperiod: 23.93,
-//         period: 1,
-//         radiuse: 6378,
-//         satellites: 1,
-//         au: 1,
-//         eccentricity: 0.017,
-//         velocity: 29.79,
-//         mass: 1,
-//         inclination: 0
-//     },
-//     Mars:
-//     {
-//         radiusp: 3380,
-//         name: 'Mars',
-//         density: 3.95,
-//         tilt: 25.19,
-//         image: 'textures/mars.gif',
-//         rotationperiod: 24.62,
-//         period: 1.88,
-//         radiuse: 3397,
-//         satellites: 2,
-//         au: 1.524,
-//         eccentricity: 0.093,
-//         velocity: 24.14,
-//         mass: 0.11,
-//         inclination: 1.85
-//     },
-//     Jupiter:
-//     {
-//         radiusp: 66854,
-//         name: 'Jupiter',
-//         density: 1.33,
-//         tilt: 3.12,
-//         image: 'textures/jupiter.gif',
-//         rotationperiod: 9.92,
-//         period: 11.86,
-//         radiuse: 71490,
-//         satellites: 69,
-//         au: 5.203,
-//         eccentricity: 0.048,
-//         velocity: 13.06,
-//         mass: 317.89,
-//         inclination: 1.3
-//     },
-//     Saturn:
-//     {
-//         radiusp: 54360,
-//         name: 'Saturn',
-//         density: 0.69,
-//         tilt: 26.73,
-//         image: 'textures/saturn.gif',
-//         rotationperiod: 10.66,
-//         period: 29.46,
-//         radiuse: 60268,
-//         satellites: 30,
-//         au: 9.539,
-//         eccentricity: 0.056,
-//         velocity: 9.64,
-//         mass: 95.18,
-//         inclination: 2.49
-//     },
-//     Uranus:
-//     {
-//         radiusp: 24973,
-//         name: 'Uranus',
-//         density: 1.29,
-//         tilt: 97.86,
-//         image: 'textures/uranus.gif',
-//         rotationperiod: 17.24,
-//         period: 84.01,
-//         radiuse: 25559,
-//         satellites: 24,
-//         au: 19.19,
-//         eccentricity: 0.046,
-//         velocity: 6.81,
-//         mass: 14.53,
-//         inclination: 0.77
-//     },
-//     Neptune:
-//     {
-//         radiusp: 24340,
-//         name: 'Neptune',
-//         density: 1.64,
-//         tilt: 29.6,
-//         image: 'textures/neptune.gif',
-//         rotationperiod: 16.11,
-//         period: 164.79,
-//         radiuse: 25269,
-//         satellites: 8,
-//         au: 30.06,
-//         eccentricity: 0.01,
-//         velocity: 5.43,
-//         mass: 17.14,
-//         inclination: 1.77
-//     },
-//     Pluto:
-//     {
-//         radiusp: 'same',
-//         name: 'Pluto',
-//         density: 2.03,
-//         tilt: 122.46,
-//         image: 'textures/pluto.gif',
-//         rotationperiod: 153.3,
-//         period: 248.54,
-//         radiuse: 1160,
-//         satellites: 1,
-//         au: 39.48,
-//         eccentricity: 0.248,
-//         velocity: 4.74,
-//         mass: 0.002,
-//         inclination: 17.15
-//     }
-// }
+const system = {
+    Mercury:
+    {
+        radiusp: 'same ',
+        name: 'Mercury',
+        density: 5.43,
+        tilt: 2,
+        image: 'textures/mercury.gif',
+        rotationperiod: 1408,
+        period: 0.24,
+        radiuse: 2439,
+        satellites: 0,
+        au: 0.3871,
+        eccentricity: 0.206,
+        velocity: 47.89,
+        mass: 0.06,
+        inclination: 7
+    },
+    Venus:
+    {
+        radiusp: 'same ',
+        name: 'Venus',
+        density: 5.25,
+        tilt: 177.3,
+        image: 'textures/venus.gif',
+        rotationperiod: 5832,
+        period: 0.62,
+        radiuse: 6052,
+        satellites: 0,
+        au: 0.7233,
+        eccentricity: 0.007,
+        velocity: 35.04,
+        mass: 0.82,
+        inclination: 3.4
+    },
+    Earth:
+    {
+        radiusp: 6357,
+        name: 'Earth',
+        density: 5.52,
+        tilt: 23.45,
+        image: 'textures/earth.gif',
+        rotationperiod: 23.93,
+        period: 1,
+        radiuse: 6378,
+        satellites: 1,
+        au: 1,
+        eccentricity: 0.017,
+        velocity: 29.79,
+        mass: 1,
+        inclination: 0
+    },
+    Mars:
+    {
+        radiusp: 3380,
+        name: 'Mars',
+        density: 3.95,
+        tilt: 25.19,
+        image: 'textures/mars.gif',
+        rotationperiod: 24.62,
+        period: 1.88,
+        radiuse: 3397,
+        satellites: 2,
+        au: 1.524,
+        eccentricity: 0.093,
+        velocity: 24.14,
+        mass: 0.11,
+        inclination: 1.85
+    },
+    Jupiter:
+    {
+        radiusp: 66854,
+        name: 'Jupiter',
+        density: 1.33,
+        tilt: 3.12,
+        image: 'textures/jupiter.gif',
+        rotationperiod: 9.92,
+        period: 11.86,
+        radiuse: 71490,
+        satellites: 69,
+        au: 5.203,
+        eccentricity: 0.048,
+        velocity: 13.06,
+        mass: 317.89,
+        inclination: 1.3
+    },
+    Saturn:
+    {
+        radiusp: 54360,
+        name: 'Saturn',
+        density: 0.69,
+        tilt: 26.73,
+        image: 'textures/saturn.gif',
+        rotationperiod: 10.66,
+        period: 29.46,
+        radiuse: 60268,
+        satellites: 30,
+        au: 9.539,
+        eccentricity: 0.056,
+        velocity: 9.64,
+        mass: 95.18,
+        inclination: 2.49
+    },
+    Uranus:
+    {
+        radiusp: 24973,
+        name: 'Uranus',
+        density: 1.29,
+        tilt: 97.86,
+        image: 'textures/uranus.gif',
+        rotationperiod: 17.24,
+        period: 84.01,
+        radiuse: 25559,
+        satellites: 24,
+        au: 19.19,
+        eccentricity: 0.046,
+        velocity: 6.81,
+        mass: 14.53,
+        inclination: 0.77
+    },
+    Neptune:
+    {
+        radiusp: 24340,
+        name: 'Neptune',
+        density: 1.64,
+        tilt: 29.6,
+        image: 'textures/neptune.gif',
+        rotationperiod: 16.11,
+        period: 164.79,
+        radiuse: 25269,
+        satellites: 8,
+        au: 30.06,
+        eccentricity: 0.01,
+        velocity: 5.43,
+        mass: 17.14,
+        inclination: 1.77
+    },
+    Pluto:
+    {
+        radiusp: 'same',
+        name: 'Pluto',
+        density: 2.03,
+        tilt: 122.46,
+        image: 'textures/pluto.gif',
+        rotationperiod: 153.3,
+        period: 248.54,
+        radiuse: 1160,
+        satellites: 1,
+        au: 39.48,
+        eccentricity: 0.248,
+        velocity: 4.74,
+        mass: 0.002,
+        inclination: 17.15
+    }
+}
 
 // // Print the mass of Mercury
 // console.log((`The mass of Mercury ${system.Mercury.mass}`));
@@ -266,9 +266,16 @@
 // const venusEccentricity = system.Venus.eccentricity;
 // console.log(`the eccentricity of Venus is ${venusEccentricity}`);
 
-// // Print the sum of the number of satellites of Jupiter, Saturn, Uranus, and Neptune (131)
-// const sumSatellites = system.Jupiter.satellites + system.Saturn.satellites + system.Uranus.satellites + system.Neptune.satellites;
-// console.log(`The sume of the number of satellites of Jupiter, Saturn, Uranus, and Neptune is ${sumSatellites}`);
+// Print the sum of the number of satellites of Jupiter, Saturn, Uranus, and Neptune (131)
+//new solution
+const somePlanets = ['Jupiter', 'Saturn', 'Uranus', 'Neptune']
+let sumSatellites = 0;
+for (let planet of somePlanets) {
+    console.log(planet, system[planet]);
+    sumSatellites += system[planet].satellites;
+}
+// old solution: const sumSatellites = system.Jupiter.satellites + system.Saturn.satellites + system.Uranus.satellites + system.Neptune.satellites;
+console.log(`The sume of the number of satellites of Jupiter, Saturn, Uranus, and Neptune is ${sumSatellites}`);
 
 // // Write a conditional that checks if Mercury's radiusp is a string (remember the typeof command). If Mercury's radiusp is a string, change the value to be the same as its radiuse value. (radiusp is radius at the poles, radiuse is radius at the equator).
 // let mercuryRadiusp = system.Mercury.radiusp;
@@ -307,46 +314,46 @@
 // system.Sophie = planetX;
 // console.log(system);
 
-// The Reading List
-const books = [
-    {
-        title: 'How not to be wrong',
-        author: 'Jordan Ellenberg',
-        alreadyRead: false
-    },
-    {
-        title: 'Quiet',
-        author: 'Susan Cain',
-        alreadyRead: false
-    },
-    {
-        title: 'Smarter, Faster, Better',
-        author: 'Charles Duhigg',
-        alreadyRead: true
-    },
-    {
-        title: 'David and Goliath',
-        author: 'Malcolm Gladwell',
-        alreadyRead: false
-    },
-    {
-        title: 'To Kill a Mockingbird',
-        author: 'Harper Lee',
-        alreadyRead: false
-    }
-];
+// // The Reading List
+// const books = [
+//     {
+//         title: 'How not to be wrong',
+//         author: 'Jordan Ellenberg',
+//         alreadyRead: false
+//     },
+//     {
+//         title: 'Quiet',
+//         author: 'Susan Cain',
+//         alreadyRead: false
+//     },
+//     {
+//         title: 'Smarter, Faster, Better',
+//         author: 'Charles Duhigg',
+//         alreadyRead: true
+//     },
+//     {
+//         title: 'David and Goliath',
+//         author: 'Malcolm Gladwell',
+//         alreadyRead: false
+//     },
+//     {
+//         title: 'To Kill a Mockingbird',
+//         author: 'Harper Lee',
+//         alreadyRead: false
+//     }
+// ];
 
-//Log the book and its author
-for (let book of books) {
-    console.log(`${book.title} by ${book.author}`);
-}
+// //Log the book and its author
+// for (let book of books) {
+//     console.log(`${book.title} by ${book.author}`);
+// }
 
-//Update code above
-for (let book of books) {
-    if (book.alreadyRead === true) {
-        console.log(`I have already read ${book.title} by ${book.author}`);
-    } else {
-        console.log(`I want to read ${book.title} by ${book.author}.`);
+// //Update code above
+// for (let book of books) {
+//     if (book.alreadyRead === true) {
+//         console.log(`I have already read ${book.title} by ${book.author}`);
+//     } else {
+//         console.log(`I want to read ${book.title} by ${book.author}.`);
 
-    }
-}
+//     }
+// }
