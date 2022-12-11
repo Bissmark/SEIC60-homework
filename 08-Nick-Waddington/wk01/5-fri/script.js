9c107ffa42e0a812e1007347913963b2cbbb0ae5
waddo4 <109324137+waddo4@users.noreply.github.com>
2022-12-11 15:23:31 +1100

    basic function showing line stops

--- 08-Nick-Waddington/wk01/5-fri/script.js
+++ 08-Nick-Waddington/wk01/5-fri/script.js
@@ -1,1 +1,23 @@
-alert ("hello")
+const subwayLines = {
+
+    Nline: ['Times Square', '34th', '28th_N', '23rd', 'Union Square_N', '8th'],
+
+    Lline: ['8th', '6th', 'Union Square_L', '3rd', '1st'],
+
+    Sixline: ['Grand Central', '33rd', '28th_L', '23rd', 'Union Square_S', 'Astor Place'],
+
+}
+
+const subwaySystem = function (lineStart, start, lineEnd, end) {
+
+    const lineStops = subwayLines.Nline.join(', ')
+
+    for (i=0; i<lineStart.length;i++){
+
+        return console.log(`You must travel through the following stops on the ${lineStart} Line: ${lineStops}.`);
+        }
+}
+
+subwaySystem ('N', 'Times Square', '6', '33rd')
+
+
