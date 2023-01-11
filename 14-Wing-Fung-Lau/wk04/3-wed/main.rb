9cc84166c4868fe9b22c452e93b769698d11dbed
Wing Fung Lau <wingfunglau@192-168-1-101.tpgi.com.au>
2023-01-11 22:18:15 +1100

    delete unnecessary lines

--- 14-Wing-Fung-Lau/wk04/3-wed/main.rb
+++ 14-Wing-Fung-Lau/wk04/3-wed/main.rb
@@ -12,33 +12,30 @@ def book_search(title)
     publisher_index = info.index('"publisher": ') + '"publisher": '.length
     date_index = info.index('publishedDate": ') + 'publishedDate": '.length
   
-    @book_title = info[title_index + 1 .. info[title_index .. ].index('",') + title_index - 1]
+    book_title = info[title_index + 1 .. info[title_index .. ].index('",') + title_index - 1]
     
-    @book_cover = info[thumbnail_index + 1 .. info[thumbnail_index .. ].index('},') + thumbnail_index - 11]
+    book_cover = info[thumbnail_index + 1 .. info[thumbnail_index .. ].index('},') + thumbnail_index - 11]
 
-    @publisher = info[publisher_index + 1 .. info[publisher_index .. ].index('",') + publisher_index - 1].gsub('"', '').to_s.split(" ").map(&:capitalize).join(" ").strip
+    publisher = info[publisher_index + 1 .. info[publisher_index .. ].index('",') + publisher_index - 1].gsub('"', '').to_s.split(" ").map(&:capitalize).join(" ").strip
 
-    @authors = info[authors_index + 1 .. info[authors_index .. ].index('],') + authors_index - 1].gsub('"', '').sub(@publisher,'').strip
+    authors = info[authors_index + 1 .. info[authors_index .. ].index('],') + authors_index - 1].gsub('"', '').sub(publisher,'').strip
 
-    @authors = @authors.chop if @authors[-1] == ','
+    authors = authors.chop if authors[-1] == ','
 
-    @date = info[date_index + 1 .. info[date_index .. ].index('",') + date_index - 1].gsub('"', '').strip
+    date = info[date_index + 1 .. info[date_index .. ].index('",') + date_index - 1].gsub('"', '').strip
     
-    if @authors.include? ','
-        @authors_form = "Authors"
+    if authors.include? ','
+        authors_form = "Authors"
     else
-        @authors_form = "Author"
+        authors_form = "Author"
     end
     "<container>
-        <img src=#{@book_cover}>
+        <img src=#{book_cover}>
         <div id='book_info'>
-            <p>Title: #{@book_title}</p>
-
-            <p>#{@authors_form}: #{@authors}</p>
-
-            <p>Publisher: #{@publisher}</p>
-
-            <p>Published date: #{@date}</p>
+            <p>Title: #{book_title}</p>
+            <p>#{authors_form}: #{authors}</p>
+            <p>Publisher: #{publisher}</p>
+            <p>Published date: #{date}</p>
         </div>
     </container>"
 end
