af4090d4fa64bc6e9dbc68d3ce65df6f7640c606
waddo4 <109324137+waddo4@users.noreply.github.com>
2023-01-16 23:58:08 +1100

    made it look pretty

--- 08-Nick-Waddington/wk05/1-mon/games_on_rails/app/controllers/number_controller.rb
+++ 08-Nick-Waddington/wk05/1-mon/games_on_rails/app/controllers/number_controller.rb
@@ -4,14 +4,26 @@ class NumberController < ApplicationController
     end
 
     def answer
-        random_number = rand(10)
+        @random_number = rand(10)
         guess = params[:guess].to_i
 
-        if guess == random_number
-            @answer = "yay"
-        elsif guess != random_number
-            @answer = "try again"
+        if guess == @random_number
+            @answer = "Your guess pleases the spirits."
+        elsif guess != @random_number
+            @answer = "The spirits say the correct number is #{ @random_number }. You lose."
        end
+
+        if guess == 1 || guess == 6
+            @mask = "/assets/mask1.jpg"
+        elsif guess == 2 || guess == 7
+            @mask = "/assets/mask2.jpg"
+        elsif guess == 0 || guess == 3 || guess == 8
+            @mask = "/assets/mask3.jpg"
+        elsif guess == 4 || guess == 9
+            @mask = "/assets/mask4.png"
+        elsif guess == 5 || guess == 10
+            @mask = "/assets/mask5.jpg"
+        end
     end
 end
 
--- 08-Nick-Waddington/wk05/1-mon/games_on_rails/app/controllers/rps_controller.rb
+++ 08-Nick-Waddington/wk05/1-mon/games_on_rails/app/controllers/rps_controller.rb
@@ -7,15 +7,20 @@ class RpsController < ApplicationController
         comp_choice = ['rock', 'paper', 'scissors'].sample
 
         if params[:guess] == comp_choice
-            @result = "draw"
+            @result = "Draw"
+            @yoshi = "/assets/big_yoshi1.jpg"
         elsif params[:guess] == 'rock' && comp_choice == 'paper'
-            @result = "Computer wins"
+            @result = "Big Yoshi wins"
+            @yoshi = "/assets/big_yoshi2.jpg"
         elsif params[:guess] == 'paper' && comp_choice == 'scissors'
-            @result = "Computer wins"
+            @result = "Big Yoshi wins"
+            @yoshi = "/assets/big_yoshi2.jpg"
         elsif params[:guess] == 'scissors' && comp_choice == 'rock'
-            @result = "Computer wins"
+            @result = "Big Yoshi wins"
+            @yoshi = "/assets/big_yoshi2.jpg"
         else
-            @result = "Player wins"
+            @result = "You win"
+            @yoshi = "/assets/big_yoshi3.png"
         end
     end
 
