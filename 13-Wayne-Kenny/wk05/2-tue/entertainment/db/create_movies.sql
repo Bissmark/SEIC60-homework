87d518a359f5b7c1d177b59ba1079b205eab45d3
Wayne Kenny <waynekenny@Waynes-MacBook-Air.local>
2023-01-18 22:25:31 +1100

    ignore the shocking css

---
+++ 13-Wayne-Kenny/wk05/2-tue/entertainment/db/create_movies.sql
@@ -0,0 +1,9 @@
+CREATE TABLE movies (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    poster TEXT,
+    director TEXT,
+    year INTEGER,
+    age_rating TEXT,
+    imdb_rating FLOAT
+);
