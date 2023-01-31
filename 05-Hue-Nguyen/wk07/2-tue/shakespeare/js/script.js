199738a9a80a553f71ba62ae79ada34c44580a41
HueNguyen <nguyenhue.se@gmail.com>
2023-01-31 15:54:36 +1100

    done

---
+++ 05-Hue-Nguyen/wk07/2-tue/shakespeare/js/script.js
@@ -0,0 +1,11 @@
+const fetchData = function () {
+    // get the value of "play" option
+    const choice = $('#plays').val();
+    // request the data based on the play option
+    $.ajax(`https://www.folgerdigitaltexts.org/${choice}/synopsis/`).done(function (data) {
+    // show the play synopsis
+    $('.synopsis').append(data);
+    })
+};
+
+$('#read').on('click', fetchData)
