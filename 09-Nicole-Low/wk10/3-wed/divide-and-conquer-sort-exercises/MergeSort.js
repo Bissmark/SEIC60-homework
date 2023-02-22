0d9c90186263ed275c9a6b4e4b95d47da4443ccf
Nicole Low <nicolexylow@gmail.com>
2023-02-22 23:19:09 +1100

    wk10d2 and wk10d3 homework submission

---
+++ 09-Nicole-Low/wk10/3-wed/divide-and-conquer-sort-exercises/MergeSort.js
@@ -0,0 +1,27 @@
+function mergeSort(arr) {
+  // YOUR CODE HERE
+
+  // recursion
+  if (arr.length === 1) {
+    return arr
+  }
+
+  mergeSort(arr)
+}
+
+// HELPER FUNCTION: merge two sorted arrays
+function merge(arr1, arr2) {
+  var result = [];
+
+  while (arr1.length && arr2.length) {
+    if(arr1[0] <= arr2[0]) {
+      result.push(arr1.shift());
+    } else {
+      result.push(arr2.shift());
+    }
+  }
+
+  return result.concat(arr1, arr2);
+}
+
+module.exports = mergeSort;
