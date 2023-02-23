4976f33322a579dffe529d243f36a059c593056b
Nicole Low <nicolexylow@gmail.com>
2023-02-24 09:02:57 +1100

    updated wk10d4 homework

--- 09-Nicole-Low/wk10/4-thu/search-algorithms/searches.js
+++ 09-Nicole-Low/wk10/4-thu/search-algorithms/searches.js
@@ -11,23 +11,33 @@ function binarySearch(arr, element){
     return -1
 }
 
-function recursiveBinarySearch(arr, element, low, high){
+function recursiveBinarySearch(arr, element) {
+    return recursiveBinarySearch2(arr, 0, arr.length - 1, element)
+}
+
+function recursiveBinarySearch2(arr, left, right, element){
     //  search through the array recursively for the element
     //  you may need to add more parameters to this function!
     //  if the element is not found, return -1
     //  if the element is found, return the index at which it was found
 
     // sort the array first
-    arrCopy = arr
+    arr.sort()
 
     // base case
-    if (arr.length === 0) {
+    if (left > right) {
         return -1
     }
 
-    const middleElem = Math.floor(arr.length / 2)
-    if (middleElem === element) {
-        return middleElem
+    // find the middle
+    const mid = left + ((right - left) / 2)
+
+    if (arr[mid] === element) {
+        return mid
+    } else if (arr[mid] < element) {
+        return recursiveBinarySearch2(arr, mid + 1, right, element)
+    } else {
+        return recursiveBinarySearch2(arr, left, mid - 1, element)
     }
 }
 
